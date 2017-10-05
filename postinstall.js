5256527cf53de5d7333e84171a032e1197c835e1
Gilad Foyer <gilad.foyer@gmail.com>
2017-10-05 12:20:58 +0300

    Fix lint errors (#10)

--- postinstall.js
+++ postinstall.js
@@ -14,14 +14,14 @@ exec('which wget', function(err,stdout,stderr){
 			if(!stdout){
 				console.log('Automatic installation isn\'t supported on your system');
 				console.log('Please install wget using your package manager');
-				process.exit(1)
+				process.exit(1);
 			}
 			else{
 				console.log('wget can be automatically installed on your system using apt-get');
 				console.log('You may need to enter your password');
 				rl.question('Would you like to install wget? [Y/n]', function(answer){
 					if (answer.match(/^y/i)){
-						var installation = spawn('sudo', ['apt-get', 'install', 'wget', '-y'])
+						var installation = spawn('sudo', ['apt-get', 'install', 'wget', '-y']);
 						installation.stdout.on('data', function(data){
 							process.stdout.write(data);
 						});
